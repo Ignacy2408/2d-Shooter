98b6b69f0556fd749e6c050278c6cc9fbd14e315
Ignacy2408 <ignacy204@gmail.com>
2024-02-24 23:00:26 +0100

    p1andmap
    Player 1 und Map

---
+++ scenes/Player1.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -750.0
+@onready var sprite_2d = $Sprite2D
+@export var changeSpeed = 150
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	#Animaitons
+	
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else: 
+		sprite_2d.animation = "default"
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
+
+	# Handle jump.
+	if Input.is_action_just_pressed("p1Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("p1Left", "p1Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, changeSpeed)
+
+	move_and_slide()
+	
+	#var isLeft = velocity.x < 0
+	#sprite_2d.flip_h = isLeft
+	
+	if (velocity.x < 0):
+		sprite_2d.flip_h = true
+	elif (velocity.x > 0): 
+		sprite_2d.flip_h = false

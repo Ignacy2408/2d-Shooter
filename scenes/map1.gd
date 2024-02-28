088af72f057a44778fb7a75ef1420ab400a94c5d
Ignacy2408 <ignacy204@gmail.com>
2024-02-28 01:47:43 +0100

    maps

--- scenes/map1.gd
+++ scenes/map1.gd
@@ -1,7 +1,4 @@
 extends Node2D
 
 
-func _process(_delta):
-	if Input.is_action_just_pressed("Quit"):
-		get_tree().quit()
-		
+
--- scenes/maps.gd
+++ scenes/maps.gd
@@ -1,2 +1,6 @@
 extends Node2D
 
+func _process(_delta):
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().quit()
+		

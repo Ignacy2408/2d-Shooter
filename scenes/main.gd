4ee6ca6e4b35bb470926cd97dcf92c256eb51bc1
Benji1600 <bwinter1604@gmail.com>
2024-03-04 23:03:11 +0100

    kleiner Settings Step

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,3 +8,7 @@ func _on_quit_button_pressed():
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/maps.tscn")
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")

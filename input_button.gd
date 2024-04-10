8007c481e575778ac57b3315167ae4bacd1bb226
Benji1600 <bwinter1604@gmail.com>
2024-04-10 18:37:22 +0200

    Settings

---
+++ input_button.gd
@@ -0,0 +1,34 @@
+extends Button
+
+
+@onready var input_button_scene = preload("res://input_button.tscn")
+@onready var action_list = $PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/ActionList
+
+
+var is_remapping = false
+var action_to_remap = null
+var remapping_button = null
+
+
+func _ready():
+	_create_action_list()
+
+
+func _create_action_list():
+	InputMap.load_from_project_settings()
+	for item in action_list.get_children():
+		item.queue_free()
+	
+	for action in InputMap.get_actions():
+		var button = input_button_scene.instantiate()
+		var action_label = button.find_child("LabelAction")
+		var input_label = button.find_child("LabelInput")
+		
+		action_label.text = action
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			input_label.text = events[0].as_text()
+		else:
+			input_label
+	

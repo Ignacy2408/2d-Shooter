a612935ecabefef794417f0edfd1c05d55559a61
Ignacy2408 <ignacy204@gmail.com>
2024-04-13 15:30:46 +0200

    bullet

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
+
 @export var bullet_speed = 500
 var right = true
+var direction = Vector2.ZERO
 func _ready(): 
 	pass
 
 func _process(delta):
+
 	if right == true:
 		self.position.x +=  bullet_speed * delta
+
 	elif right == false:
 		self.position.x += -1 * bullet_speed * delta
+	
+		
+
+		

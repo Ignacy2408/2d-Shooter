79f8f566ea7da7727b387b76621b2f00dac211b9
Benji1600 <bwinter1604@gmail.com>
2024-04-14 18:56:50 +0200

    Update main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -13,4 +13,4 @@ func _on_play_button_pressed():
 
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+	get_tree().change_scene_to_file("res://scenes/InputSettings.tscn")

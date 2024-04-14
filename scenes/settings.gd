97cecfeecce180a08c7032d2d74ba16803338815
Benji1600 <bwinter1604@gmail.com>
2024-04-14 21:13:48 +0200

    Settings fertig gemacht

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -39,9 +39,44 @@ func _create_action_list():
 		
 		var events = InputMap.action_get_events(action)
 		if events.size() > 0:
-			input_label.text = events[0].as_text()
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
 		else:
 			input_label.text = ""
 		
 		action_list.add_child(button)
-	
+		button.pressed.connect(_on_input_button_pressed.bind(button, action))
+
+
+func _on_input_button_pressed(button, action):
+	if !is_remapping:
+		is_remapping = true
+		action_to_remap = action
+		remapping_button = button
+		button.find_child("LabelInput").text = "Press key to bind..."
+
+func _input(event):
+	if is_remapping:
+		if (
+			event is InputEventKey ||
+			(event is InputEventMouseButton && event.pressed)
+		):
+			# doppel klick soll ein klick sein
+			if event is InputEventMouseButton && event.double_click:
+				event.double_click = false
+			
+			InputMap.action_erase_events(action_to_remap)
+			InputMap.action_add_event(action_to_remap, event)
+			_update_action_list(remapping_button, event)
+			
+			is_remapping = false
+			action_to_remap = null
+			remapping_button = null
+			
+			accept_event()
+
+func _update_action_list(button, event):
+	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
+
+
+func _on_reset_button_pressed():
+	_create_action_list()

18588927bed9a488ea4bf82a83301f2a0906b4b2
Benji1600 <bwinter1604@gmail.com>
2024-04-18 16:27:42 +0200

    Signale für die Maps

--- scenes/map_wahl.gd
+++ scenes/map_wahl.gd
@@ -8,17 +8,22 @@ func _on_back_button_pressed():
 
 
 func _on_map_1_button_pressed():
+	Signals.emit_signal("map1")
 	get_tree().change_scene_to_file("res://scenes/maps.tscn")
 
 
+
 func _on_map_2s_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/maps2.tscn")
+	get_tree().change_scene_to_file("res://scenes/maps.tscn")
+	Signals.emit_signal("map2")
 
 
 func _on_map_3_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/maps3.tscn")
+	Signals.emit_signal("map3")
 
 
 func _on_map_4_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/maps4.tscn")
+	Signals.emit_signal("map4")
 
--- scenes/signals/signals.gd
+++ scenes/signals/signals.gd
@@ -1,3 +1,7 @@
 extends Node
 
 signal con
+signal map1
+signal map2
+signal map3
+signal map4
